9c8057069486c85b3a157dfc18add7a73fba9f30
hoducanh <hoducanh0232001@gmail.com>
2022-06-27 15:16:23 +0700

    ducanh

--- src/components/PostFiltersForm/Casting.js
+++ src/components/PostFiltersForm/Casting.js
@@ -1,27 +1,37 @@
-
-function convertTimestamp(timestamp) {
-var d = new Date(timestamp * 1000), // Convert timestamp to ms
-    yyyy = d.getFullYear(),
-    mm = ('0' + (d.getMonth() + 1)).slice(-2), 
-    dd = ('0' + d.getDate()).slice(-2),        
-    hh = d.getHours(),
-    h = hh,
-    min = ('0' + d.getMinutes()).slice(-2),   
-    ampm = 'AM',
-    time;
-
-if (hh > 12) {
-    h = hh - 12;
-    ampm = 'PM';
-} else if (hh === 12) {
-    h = 12;
-    ampm = 'PM';
-} else if (hh == 0) {
-    h = 12;
-}
-
-// ie: 2014-03-24, 3:00 PM
-time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
-return time;
-}
-
+export function convertTimestamp(timestamp) {
+    var d = new Date(timestamp * 1000), // Convert timestamp to ms
+        yyyy = d.getFullYear(),
+        mm = ('0' + (d.getMonth() + 1)).slice(-2), 
+        dd = ('0' + d.getDate()).slice(-2),        
+        hh = d.getHours(),
+        h = hh,
+        min = ('0' + d.getMinutes()).slice(-2),   
+        ampm = 'AM',
+        time;
+    
+    // if (hh > 12) {`
+    //     h = hh - 12;
+    //     ampm = 'PM';
+    // } else if (hh === 12) {
+    //     h = 12;
+    //     ampm = 'PM';
+    // } else if (hh == 0) {
+    //     h = 12;
+    // }
+    
+    // ie: 2014-03-24, 3:00 PM
+    time = yyyy + '-' + mm + '-' + dd + ', ' + hh + ':' + min + ' ' //+ ampm // edit time to 24h format
+    return time;
+    }
+// export function convertVehicle(vehicle){
+//      new String(vehicle);
+//     if(vehicle == 3){
+//         console.log("Xe moto 2 banh");
+//     }
+//     else if(vehicle == 4){
+//         console.log("Xe oto");
+//     }else if(vehicle == 5){
+//         console.log("Xe oto dien");
+//     }
+//     return vehicle;
+// }
--- src/components/PostFiltersForm/index.jsx
+++ src/components/PostFiltersForm/index.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import axios from 'axios';
-import {convertTimestamp} from 'Casting/convertTimestamp';
+import {convertTimestamp} from './Casting'
 
 PostFiltersForm.propTypes ={
 onSubmit: PropTypes.func,
@@ -66,23 +66,42 @@ function PostFiltersForm() {
         <tbody>
             {
                 <tr key = {parking.id}>
-                    <td>{parking.id}</td>
-                    <td>{parking.parkingCode}</td>
-                    <td>{parking.vechile_cat_id}</td>
-                    <td>{parking.rate_id}</td>
-                    <td>{parking.slot_id}</td>
-                    <td>
-                      <script >
-                      
-                      Casting.convertTimestamp('parking.in_time')
-                      </script>
-                      {parking.in_time}</td>
-                    <td>{parking.out_time}</td>
-                    <td>{parking.total_time}</td>
-                    <td>{parking.earned_amount}</td>
-                    <td>{parking.paid_status}</td>
+                <td>{parking.id}</td>
+                <td>{parking.parkingCode}</td>
+                <td>{parking.vechile_cat_id ==3 ?<console>Xe 2 bánh</console> :null}
+                {parking.vechile_cat_id ==4 ?<console>Xe oto</console> :null}
+                {parking.vechile_cat_id ==5 ?<console>Xe oto điện</console> :null}
+                </td>
+                <td>{parking.rate_id ==7 ? <console>Xe điện có trạm sạc</console> :null}
+                {parking.rate_id ==8 ? <console>Xe gắn máy</console> :null}
+                {parking.rate_id ==9 ? <console>Xe ô tô ngoài trời</console> :null}
+                {parking.rate_id ==10 ? <console>Xe gắn máy giờ cố định</console> :null}
+                {parking.rate_id ==11 ? <console>Xe ô tô có mái che theo giờ</console> :null}
 
-                </tr>
+                </td>
+                <td>{parking.slot_id ==1 ? <console>TW1</console> :null}
+                {parking.slot_id ==3 ? <console>B1</console> :null}
+                {parking.slot_id ==6 ? <console>TW2</console> :null}
+                {parking.slot_id ==7 ? <console>B2</console> :null}
+                {parking.slot_id ==8 ? <console>B3</console> :null}
+                {parking.slot_id ==9 ? <console>B4</console> :null}
+                {parking.slot_id ==10 ? <console>SS8</console> :null}
+                {parking.slot_id ==11 ? <console>B12</console> :null}
+                {parking.slot_id ==12 ? <console>A1 có trạm sạc</console> :null}
+                </td>
+                <td>
+                {convertTimestamp(parking.in_time)}
+                  </td>
+                <td>
+                {parking.out_time ?convertTimestamp(parking.out_time): null}
+                
+                 </td>
+                <td>{parking.total_time}</td>
+                <td>{parking.earned_amount}</td>
+                <td>{parking.paid_status == 1 ? <console class="label label-success">Paid</console> : <console class="label label-danger">Not Paid</console>}
+                </td>
+
+            </tr>
              
         }
         </tbody>
